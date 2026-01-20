7e12c8ffdb638a7ea8d7114204473b818da69636
Chard Vinas <chard.vinas@gmail.com>
2026-01-20 11:28:48 +1100

    streamlit description changes

--- home.py
+++ home.py
@@ -19,9 +19,8 @@ if ui_constants.SERVICE_STORAGE not in st.session_state:
         st.session_state[ui_constants.SERVICE_STORAGE] = StorageService([constants.INPUT_BUCKET, constants.OUTPUT_BUCKET], constants.SERVICE_ACCOUNT_KEY_FILE)
 
 header_row = st.columns([6,1], vertical_alignment="center")
-header_row[0].title("Editorial Solaris - A metadata explorer")
+header_row[0].title("Metadata explorer")
 
 # Page definition
-st.write("Editorial Solaris, powered by Google's Gemini, leverages its multimodal capabilities to revolutionize digital content management. By seamlessly combining information from audio and video, Gemini generates rich metadata that allows users to effortlessly search, organize, and explore their digital libraries with unprecedented ease and efficiency.  This means you can use the metadata to quickly and easily find the files you're looking for, even if you have a huge collection of audio and video content.")
-st.write("All the code can be found in this github rep [https://github.com/pedroccorreia/metadata_explorer/](rhttps://github.com/pedroccorreia/metadata_explorer/)")
+st.write("Editorial Solaris, leverages LLM multimodal capabilities to revolutionize digital content management. By seamlessly combining information from audio and video, AI generates rich metadata that allows users to effortlessly search, organize, and explore their digital libraries with unprecedented ease and efficiency.  This means you can use the metadata to quickly and easily find the files you're looking for, even if you have a huge collection of audio and video content.")
 
